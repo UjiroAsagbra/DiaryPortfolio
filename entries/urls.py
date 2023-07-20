8515d623f3a774a0ed1add931cf102e4764f73cf
Ujiro Asagbra <ujiroasagbra@gmail.com>
2023-07-20 12:47:47 +0100

    initial commit

---
+++ entries/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for diary project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(
+        "",
+        views.EntryListView.as_view(),
+        name="entry-list"
+    ),
+
+    path(
+        "<int:pk>",
+        views.EntryDetailView.as_view(),
+        name="entry-detail"
+    ),
+
+    path(
+        "create",
+        views.EntryCreateView.as_view(),
+        name="entry-create"
+    ),
+    path(
+        "entry/<int:pk>/update",
+        views.EntryUpdateView.as_view(),
+        name="entry-update",
+    ),
+    path(
+        "entry/<int:pk>/delete",
+        views.EntryDeleteView.as_view(),
+        name="entry-delete",
+    ),
+
+]
